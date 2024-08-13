890b7e742f513c7de70de5871a55b1815771fbf7
Misho <chapidze.misho95@gmail.com>
2024-08-13 15:12:29 +0400

    header

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,14 +1,19 @@
 import { Outlet } from "react-router-dom";
+import Header from "./header";
 
 const Layout = () => {
   return (
-    <>
-      <header>header</header>
-      <main>
-        <Outlet />
-      </main>
+    <div className="flex flex-col justify-between min-h-screen">
+      <div className="flex justify-center">
+        <div className="w-full max-w-[1111px]">
+          <Header />
+          <main>
+            <Outlet />
+          </main>
+        </div>
+      </div>
       <footer>footer</footer>
-    </>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        PEACH: "#E7816B",
+        BLACK: "#1D1C1E",
+        LIGHT_PEACH: "#FFAD9B",
+        DARK_GREY: "#333136",
+        LIGHT_GREY: "#F1F3F5",
+      },
+      fontFamily: {
+        JOST: "Jost, sans-serif",
+      },
+    },
   },
   plugins: [],
 };
