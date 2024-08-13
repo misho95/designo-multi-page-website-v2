f9328c2febbaa3d9c9ffa05f5c0244adeeb3eae7
Misho <chapidze.misho95@gmail.com>
2024-08-13 19:40:57 +0400

    footer

---
+++ src/components/button.tsx
@@ -0,0 +1,36 @@
+import { ButtonHTMLAttributes, forwardRef } from "react";
+import { cva, VariantProps } from "class-variance-authority";
+import { cn } from "../utils/helper";
+
+interface ButtonProps
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {}
+
+const buttonVariants = cva(
+  "uppercase px-[25px] py-[18px] text-[15px] font-[500] rounded-[8px] duration-200",
+  {
+    variants: {
+      variant: {
+        dark: "bg-white text-DARK_GREY active:bg-LIGHT_PEACH active:text-white",
+        light: "bg-PEACH text-white active:bg-LIGHT_PEACH active:text-white",
+      },
+    },
+    defaultVariants: {
+      variant: "dark",
+    },
+  }
+);
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, ...props }, ref) => {
+    return (
+      <button
+        ref={ref}
+        className={cn(buttonVariants({ variant, className }))}
+        {...props}
+      />
+    );
+  }
+);
+
+export { Button, buttonVariants };
---
+++ src/components/navigation.tsx
@@ -0,0 +1,41 @@
+import { Link } from "react-router-dom";
+import { NAV_LINKS_LIST } from "../utils/data";
+import clsx from "clsx";
+
+type NavPropsType = {
+  type: "light" | "dark";
+};
+
+const Navigation = ({ type }: NavPropsType) => {
+  return (
+    <nav
+      className={clsx("flex gap-[42px]", {
+        "text-white": type === "light",
+        "text-DARK_GREY": type === "dark",
+      })}
+    >
+      {NAV_LINKS_LIST.map((link) => {
+        return (
+          <Link
+            key={link.id}
+            to={link.url}
+            className="uppercase text-[14px] leading-[14px] tracking-[2px]  group flex flex-col gap-1 items-center"
+          >
+            {link.title}
+            <span
+              className={clsx(
+                "block h-[1px] w-[0px] group-hover:w-full duration-300",
+                {
+                  "bg-white/50": type === "light",
+                  "bg-BLACK/50": type === "dark",
+                }
+              )}
+            />
+          </Link>
+        );
+      })}
+    </nav>
+  );
+};
+
+export default Navigation;
